17c3d22e5b01402cee5b34e6228a628d5fbba3c0
unknown <bilza32611@gmail.com>
2022-12-24 16:40:09 +0500

    first insert query

--- database.js
+++ database.js
@@ -1,14 +1,18 @@
  const {createPool} = require('mysql2');
+const dotEnv = require('dotenv');
+
+dotEnv.config();
 
  const pool = new createPool({
- host : "localhost",
- user : "root",
- password : "bils32611",
- database : "firstdb",
+ host : process.env.MYSQL_HOST,
+ user : process.env.MYSQL_USER,
+ password : process.env.MYSQL_PASSWORD,
+ database : process.env.MYSQL_DATABASENAME,
  connectionLimit : 10
  });
 
 
+function qry(){
  pool.query('SELECT * FROM firstdb.users', (err,result,fields)=>{
     if (err) {
     console.log("panic",err)   
@@ -17,3 +21,23 @@
     }
  
  });
+}
+ 
+
+
+function insert(){
+ pool.query('INSERT INTO firstdb.users (name,age)  VALUES ("ff" , 5)', (err,result,fields)=>{
+    if (err) {
+    console.log("panic",err)   
+    }else {
+    console.log(result);
+    }
+ 
+ });
+}
+
+
+
+
+ insert();
+ qry();
