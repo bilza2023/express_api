989abf4d62611b63510771ee12f8708c61cce818
unknown <bilza32611@gmail.com>
2023-01-03 13:01:04 +0500

    using sequlize orm have inserted records inside db next- seperate routes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const  express  =require('express');
 const cors = require('cors');
+const path = require('path');
 const PORT = process.env.PORT || 8080;
 const {db,User} = require('./database/db.js');
 
-
+// const theTblRouter = require("./routes/theTbl");
+const jwt = require('jsonwebtoken');
 ////////////////////////////////////////////////////
 const app = express()
 
@@ -11,26 +13,29 @@ const app = express()
 app.use(cors({origin:'https://localhost:8080'}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//.. Route middlewares
+// app.use("",theTblRouter);
+//.. static files
+app.use(express.static(path.join(__dirname,"public")));
+//.. Templating Engine
+app.set("view engine" , "ejs");
 ////////////////////////////////////////////////////
 
-//..Authenticate DB
-
-
-// User.create({name : "bbb44"});
-// db.then(db => {
-// db.u
-// // console.log(db);
-// });
-////////////////////////////////////////////////////
 
 app.get('/', async (req, res) =>{
-User.create( {name : "input" , email : "abc@xyz.com"} );
+
   res.status(200).send('hello world')
 });
 
-app.post('/user', function (req, res) {
-  addUser(req,res);
-})
+app.post('/addUser', function (req, res) {
+const body = req.body;
+const name = body.name;
+const email = body.email;
+// const password = body.password;
+
+const rez = User.create({ name , email } );
+res.status(200).json({ name , email });
+});
 
 app.listen(PORT);
 
