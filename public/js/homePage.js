0c6a1208a9bab1edaba6119603aef442737f50ba
unknown <bilza32611@gmail.com>
2023-01-16 13:43:57 +0500

    in the name of Allah - Home page is almost ready

--- public/js/homePage.js
+++ public/js/homePage.js
@@ -1,13 +1,4 @@
 
-const businessArray = [
-    { "name" : "Plumber"},
-    {"name" :  "Electrition"},    
-    {"name" :  "Painter"},    
-    {"name" :  "Doctor"},    
-    {"name" :  "Home-Tution"},    
-    {"name" :  "Dentist"},    
-    {"name" :  "Lawer"}    
-];
 
 
 
@@ -66,19 +57,63 @@ const citiesArray = [
 
 
 const regionsArray = [
-    { "id" : 1, "name" : "Cant" , cityId : 1},    
-    { "id" : 2, "name" : "Sadar" , cityId : 1},    
-    { "id" : 3, "name" : "Phase-1" , cityId : 1},    
-    { "id" : 4, "name" : "Gulshan-abad" , cityId : 1},    
-    { "id" : 5, "name" : "City" , cityId : 1},    
-    { "id" : 6, "name" : "Main-colony" , cityId : 2},    
-    { "id" : 7, "name" : "Bhains-coloney" , cityId : 2},    
-    { "id" : 8, "name" : "Shadman" , cityId : 2},    
-    { "id" : 9, "name" : "Sadar-link-road" , cityId : 2},    
-    { "id" : 10, "name" : "Maindan" , cityId :2},    
+    { "id" : 1, "name" : "Cant" , cityId : 1  , 
+    "plumber":396 , 
+    "electrition":336 , 
+    "dentist":66 , 
+    "lawer":65 , 
+    "home-tutor":546 , 
+    "artist":657 , 
+    "painter":446 
+    },    
+
+    { "id" : 2, "name" : "Sadar" , cityId : 1,
+    "plumber":224 , 
+    "electrition":34 , 
+    "dentist":454 , 
+    "lawer":134 , 
+    "home-tutor":14 , 
+    "artist":456 , 
+    "painter":33
+    },    
+    { "id" : 3, "name" : "Phase-1" , cityId : 1,
+    "plumber":1233 , 
+    "electrition":533 , 
+    "dentist":88 , 
+    "lawer":443 , 
+    "home-tutor":43 , 
+    "artist":34 , 
+    "painter":30
+    },    
+    { "id" : 6, "name" : "Main-colony" , cityId : 2,
+    "plumber":88 , 
+    "electrition":45 , 
+    "dentist":25 , 
+    "lawer":44 , 
+    "home-tutor":54 , 
+    "artist":56 , 
+    "painter":64
+    },    
+    { "id" : 7, "name" : "Bhains-coloney" , cityId : 2,
+    "plumber":34 , 
+    "electrition":54 , 
+    "dentist":98 , 
+    "lawer":43 , 
+    "home-tutor":433 , 
+    "artist":56 , 
+    "painter":43
+    },    
+    { "id" : 8, "name" : "Shadman" , cityId : 2,
+    "plumber":454 , 
+    "electrition":3002 , 
+    "dentist":12 , 
+    "lawer":22 , 
+    "home-tutor":55 , 
+    "artist":83 , 
+    "painter":48
+    },    
 ];
        
-
 
 
 
@@ -123,7 +158,41 @@ function updateRegions() {
       // add option to regionsDD
       regionsDD.add(option);
     }
-  }
+  }  
+    updateCards();
 }//update regionsDD
 
 
+
+////////////////////////////
+document.getElementById("regionsDD").addEventListener("change",
+updateCards);
+
+function updateCards(){
+const regionId = document.getElementById("regionsDD").value;
+    for (let i = 0; i < regionsArray.length; i++) {
+        const element = regionsArray[i];
+        if (element.id == regionId) {
+        //---------------------
+        const plumberData = document.getElementById("plumberData");
+        plumberData.innerHTML = "";
+        plumberData.innerHTML = `Available ${element.plumber}`;
+        //---------------------
+        const electritionData = document.getElementById("electritionData");
+        electritionData.innerHTML = "";
+        electritionData.innerHTML = `Available ${element.electrition}`;
+        //---------------------
+        const dentistData = document.getElementById("dentistData");
+        dentistData.innerHTML = "";
+        dentistData.innerHTML = `Available ${element.dentist}`;
+        //---------------------
+        const lawerData = document.getElementById("lawerData");
+        lawerData.innerHTML = "";
+        lawerData.innerHTML = `Available ${element.lawer}`;
+        
+        
+        }
+        
+    }
+console.log(regionId);
+}
