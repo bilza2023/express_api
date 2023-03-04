5e62d4b48ae612140f2c57d1e992928583edbb55
unknown <bilza32611@gmail.com>
2023-03-04 19:42:58 +0500

    uploading user REST API Test

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -30,9 +30,64 @@ password
 }
 });
 ////////////////////////////////////////////////////////
+userRouter.get("/" , async function(req,res) {
+  try {
 
+    const subscribers = await Subscriber.find({});
+    return res.status(200).json({msg : "success" , subscribers });
+  } catch(error) {
+    return res.status(400).json({msg : "failure" , error  });
+  }
+});
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+userRouter.get("/page/:limit?/:count?" , async function(req,res) {
+  try {
+    const { limit = 2, count = 0 } = req.params;
 
+    const subscribers = await Subscriber.find({}).limit(limit).skip(count);
+    return res.status(200).json({msg : "success" , subscribers });
+  } catch(error) {
+    return res.status(400).json({msg : "failure" , error  });
+  }
+});
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+userRouter.get("/:id" , async function(req,res) {
+  try {
+    const id= req.params.id;
 
+    const subscriber = await Subscriber.findById(id);
+    return res.status(200).json({msg : "success" , subscriber });
+  } catch(error) {
+    return res.status(400).json({msg : "failure" , error  });
+  }
+});
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+userRouter.delete("/:id" , async function(req,res) {
+  try {
+    const id= req.params.id;
+
+    await Subscriber.deleteOne({ _id: id });
+    return res.status(200).json({msg : "success" });
+  } catch(error) {
+    return res.status(400).json({msg : "failure" , error  });
+  }
+});
+////////////////////////////////////////////////////////
+userRouter.patch("/:id", async function(req, res) {
+  try {
+    const id = req.params.id;
+    const { name, email, password } = req.body;
+
+    const updatedSubscriber = await Subscriber.findByIdAndUpdate(id, { name, email, password }, { new: false });
+
+    return res.status(200).json({ msg: "success" });
+  } catch (error) {
+    return res.status(400).json({ msg: "failure", error });
+  }
+});
 //-------------------------------------------------------
 ////////////////////////////////////////////////////////
 module.exports = userRouter;
