3af645985881618b53d6ba998a2437c0cc124c37
unknown <bilza32611@gmail.com>
2023-03-07 20:54:51 +0500

    try it from server

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,6 +1,6 @@
 
 require('dotenv').config();
-
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const express = require('express');
 const userRouter = express.Router();
@@ -9,17 +9,48 @@ const Subscriber = require("../models/subscriber");
 /////////////////////////////////////////////////
 ////////-----------------CREATE---------/////////
 ////////////////////////////////////////////////
-userRouter.post("/" , async function(req,res) {
+userRouter.get("/secure" , async function(req,res) {
+ try {
+    // get token from request header
+    const token = req.headers.authorization.split(" ")[1];
+    if(token == null || token == ""){
+    res.status(404).json({message : "Auth token not found:you may not be logged in."});
+    }
+    // verify token with JWT_SECRET
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // get user id from decoded token
+    const userId = decoded.id;
+
+    // // find user by id
+    const user = await Subscriber.findById(userId);
+
+    if (user) {
+      return res.status(200).json({ msg: "success",user });
+    } else {
+      return res.status(401).json({ msg: "unauthorized" });
+    }
+
+  } catch (error) {
+    return res.status(401).json({ msg: "unauthorized" });
+  }
+
+});
+userRouter.post("/register" , async function(req,res) {
 try{
 const name = req.body.name;
 const email = req.body.email;
-const password = req.body.password;
+const passwordPlain = req.body.password;
+
+const password = await bcrypt.hash(passwordPlain,3);
+// console.log(password);
 
 const subscriber = new  Subscriber({
 name,
 email,
 password
 });
+// console.log(subscriber);
 
         const newSub = await subscriber.save();
             return res.status(200).json({msg : "success" , newSub});
@@ -29,6 +60,47 @@ password
         return res.status(400).json({msg : "failure" , error  });
 }
 });
+
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////
+userRouter.post("/login" , async function(req,res) {
+try{
+const email = req.body.email;
+// console.log(email);
+const passwordPlain = req.body.password;
+const user = await Subscriber.findOne({email});
+  if (user == null) {
+      return res.status(200).json({msg: "can not find email address"});
+  }
+
+  if (await bcrypt.compare(passwordPlain,user.password)) {
+
+const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "12h" });
+
+res.set("Authorization", `Bearer ${token}`);
+
+
+return res.status(200).json({ msg: "compare success", token: token });
+
+
+  }else {
+      return res.status(200).json({msg: "login failed, the email and the password does not match"});
+  }
+
+}catch(error){
+        // console.log(error);
+        return res.status(400).json({msg : "failure" , error });
+}
+});
+userRouter.post("/logout" , async function(req,res) {
+try{
+res.set("Authorization", ``);
+return res.status(200).json({ msg: "compare success" });
+}catch(error){
+        return res.status(400).json({msg : "failure" , error });
+}
+});
+
 ////////////////////////////////////////////////////////
 userRouter.get("/" , async function(req,res) {
   try {
