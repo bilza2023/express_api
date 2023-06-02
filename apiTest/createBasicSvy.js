93e75a5f212be047c1c282147678dcb59e13f7ec
unknown <bilza32611@gmail.com>
2023-06-02 20:31:39 +0500

    lets see

--- apiTest/createBasicSvy.js
+++ apiTest/createBasicSvy.js
@@ -1,6 +1,6 @@
 //--The project files has all the data where as all the models are send by api.js
  
-const {getSurvey ,qDataMCQ,qDataInput,qDataParagraph,qDataEmail,qDataPassword,qDataUrl,qDataNumber} = require('../models/questionTypesData.js');
+const {getDataUrl,getDataPassword,getDataParagraph,getDataNumber,getDataInput,getDataEmail,getDataMCQ,getDataBaseMCQ, getMcqWOption, getSurvey} = require('../models/questionTypesData.js');
  
 
 async function createBasicSvy(Survey,SurveyMCQ , SurveyInput,SurveyParagraph,SurveyNumber,SurveyUrl,SurveyPassword,SurveyEmail) {
@@ -8,38 +8,38 @@ async function createBasicSvy(Survey,SurveyMCQ , SurveyInput,SurveyParagraph,Sur
 //--64202224fd8518cb214bd138 this is userId remains the same
  let survey = new Survey( getSurvey('64202224fd8518cb214bd138' , "Test...") );
 
-                const caseSurveyMCQ = new SurveyMCQ(qDataMCQ);
+                const caseSurveyMCQ = new SurveyMCQ(getDataMCQ());
                 await caseSurveyMCQ.save();
                 survey.questions.push(caseSurveyMCQ);
               ///
               
-                const caseSurveyInput = new SurveyInput(qDataInput);
+                const caseSurveyInput = new SurveyInput(getDataInput());
                 await caseSurveyInput.save();
                 survey.questions.push(caseSurveyInput);
               ////
               
-                const caseSurveyParagraph = new SurveyParagraph(qDataParagraph);
+                const caseSurveyParagraph = new SurveyParagraph(getDataParagraph());
                 await caseSurveyParagraph.save();
                 survey.questions.push(caseSurveyParagraph);
               ///
               
-                const caseSurveyEmail = new SurveyEmail(qDataEmail);
+                const caseSurveyEmail = new SurveyEmail(getDataEmail());
                 await caseSurveyEmail.save();
                 survey.questions.push(caseSurveyEmail);
               //
 
-                const caseSurveyPassword = new SurveyPassword(qDataPassword);
+                const caseSurveyPassword = new SurveyPassword(getDataPassword());
                 await caseSurveyPassword.save();
                 survey.questions.push(caseSurveyPassword);
               
               
-                const caseSurveyUrl = new SurveyUrl(qDataUrl);
+                const caseSurveyUrl = new SurveyUrl(getDataUrl());
                 await caseSurveyUrl.save();
                 survey.questions.push(caseSurveyUrl);
               //
 
               
-                const caseSurveyNumber = new SurveyNumber(qDataNumber);
+                const caseSurveyNumber = new SurveyNumber(getDataNumber());
                 await caseSurveyNumber.save();
                 survey.questions.push(caseSurveyNumber);
               //
