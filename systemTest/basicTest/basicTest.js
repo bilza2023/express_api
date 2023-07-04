b0306c7963cb8ce60f56e72da155c7e8cb64ca00
unknown <bilza32611@gmail.com>
2023-07-04 17:11:32 +0500

    base test of system with checks and 5 routes completed

--- systemTest/basicTest/basicTest.js
+++ systemTest/basicTest/basicTest.js
@@ -1,26 +1,28 @@
 const {TestData} = require('../TestData');
 //--
-const getNewTagData =  require('../../superRouters/tagFn/getTag.js');
-const checkMax =  require('../../superRouters/checks/checkMax.js');
+const getNewTagData =  require('./getData');
 const SuperRouterOptions = require('../../superRouter/superRouterOptions.js');
+const errorOther =  require('./errorOther');
+const errorSkillzaa =  require('./errorSkillzaa');
 
-//////////////////////////////////////////////////////////////
+/////////////////--From The Super Router//////////////////////
 const create = require('../../superRouter/methods/create');
 const del = require('../../superRouter/methods/delete');
 const read = require('../../superRouter/methods/read');
 const readone = require('../../superRouter/methods/readone'); 
 const update = require('../../superRouter/methods/update');
+
+// const catchFn = require('../../superRouter/coreFunctions/catchFn');
 //////////////////////////////////////////////////////////////
 //--we are going to skip SuperRouter and thus skip req,res;
 ////////////////////////////////////////////////
-const userIdVal='64202224fd8518cb214bd138';
-
  const opt = new SuperRouterOptions();
     opt.model = TestData;
     opt.debugMode = false; ///make it false after completion.
     opt.create.getNewObjDataFn = getNewTagData;
     opt.create.checks = [
-        checkMax
+        errorOther,
+        // errorSkillzaa
     ];
     opt.create.backendData = {       
             checkMaxValue : 100       
@@ -35,90 +37,38 @@ async function basicTest( ){
     console.log('\x1b[33m%s\x1b[0m',"==========================");
     console.log('\x1b[34m%s\x1b[0m',"All 5 routes");
     //////////////////////////////////////////////////////////
-
-    const itemCreated = await createTest({name: "Random Name" , description :'Random Description',userId},opt);
-    //--update needs item
-    await updateTest({item :itemCreated ,userId},opt);
-    await readTest({userId},opt);
-    await readoneTest({id :itemCreated._id,userId },opt);
-    await deleteTest({id :itemCreated._id,userId  },opt);
-
-   console.log('\x1b[36m%s\x1b[0m', 'Base Test Ended..===>>');
-}
-
-///////////////////////////////////////////////////
-
-async function createTest(data,opt){
     try{
-    //--we NEED userId
-    const itemCreated = await create(data,opt);
-    if (itemCreated){
-        console.log('\x1b[32m%s\x1b[0m','create' , itemCreated._id);
-    }else {
-        console.error('failed to create');
-    }
-return itemCreated;    
-
-    }catch(err){
-    throw err;
-    }
-}
-async function updateTest(data,opt){
-try{
- const itemUpdated = await update({item: data.item },opt);
-    if (itemUpdated){
-        console.log('\x1b[32m%s\x1b[0m','update', itemUpdated._id);
-    }else {
-        console.error('failed to update');
-    }
-return itemUpdated;    
-    }catch(err){
-    throw err;
-    }
-}
-
-async function readTest(data,opt){
-try{
- const itemRead = await read({userId : data.userId },opt);
-    if (itemRead){
-        console.log('\x1b[32m%s\x1b[0m','read: length',itemRead.length);
-    }else {
-        console.error('failed to itemRead');
-    }
-return itemRead;    
-}catch(err){
-throw err;
-}
-}
-async function readoneTest(data,opt){
-try{
- const itemReadone = await readone(data,opt);
-    if (itemReadone){
-        console.log('\x1b[32m%s\x1b[0m','readone',itemReadone._id);
-    }else {
-        console.error('failed to itemReadone');
-    }
-return itemReadone;    
-}catch(err){
-throw err;
-}
-}
-async function deleteTest(data,opt){
-try{
-const itemDeleted = await del({id :data.id,userId : data.userId},opt);
     
-    if ( itemDeleted.deletedCount < 1 ){
-        console.log('\x1b[31m%s\x1b[0m', 'failed to delete')
-    }else {
-        console.log('\x1b[35m%s\x1b[0m', 'item deleted',itemDeleted);
+    // Each method takes data,opt . just insert data into data
+    const itemCreated = await create({name: "Random Name" , description :'Random Description',userId},opt);
+    console.log('\x1b[32m%s\x1b[0m',"itemCreated" , itemCreated._id);
+
+    //--update needs item..
+    const itemUpdated = await update({item :itemCreated ,userId},opt);
+    console.log('\x1b[32m%s\x1b[0m', "itemUpdated" , itemUpdated._id);
+
+    const itemRead = await read({userId},opt);
+    console.log('\x1b[32m%s\x1b[0m',"itemRead" , itemRead.length);
+
+    const itemReadone = await readone({id :itemCreated._id,userId },opt);
+    console.log('\x1b[32m%s\x1b[0m',"itemReadone" , itemReadone._id);
+    
+    const itemDeleted = await del({id :itemCreated._id,userId  },opt);
+    console.log('\x1b[32m%s\x1b[0m',"itemDeleted" , itemDeleted);
+
+   console.log('\x1b[34m%s\x1b[0m', 'Base Test Ended..===>>');
+   
+   }catch (e) {
+
+    if (opt.debugMode){
+        console.log('\x1b[31m%s\x1b[0m',e.message );
+    }else { 
+        console.log('\x1b[31m%s\x1b[0m','operation failed' );
     }
-return itemDeleted;    
-}catch(err){
-throw err;
-}
-}
 
-////////////////////////////////
+   }
+
+} 
+///////////////////////////////////////////////////
 module.exports = basicTest
-
-
+///////////////////////////////////////////////////
